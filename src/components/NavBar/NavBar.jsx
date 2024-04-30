e6719b373bc943d5b20e07f0af9614dd0baf3320
Ganesh <ganeshrestha97@gmail.com>
2024-04-30 13:03:15 -0700

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+
+export default function NavBar({ user, setUser }) {
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
+
+  return (
+    <nav>
+      <span>Hi <strong>{user.name}</strong>!</span>
+      <br />
+      <br />
+      &nbsp;&nbsp;
+      <Link to="/orders">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+      &nbsp; | &nbsp;      
+      <Link to="" onClick={handleLogOut}>Log Out</Link>
+    </nav>
+  );
+}
